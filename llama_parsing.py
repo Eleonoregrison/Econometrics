6f8a225ac2757e4aae8d9adfe665dc4a135b10d2
Eleonoregrison <144041004+Eleonoregrison@users.noreply.github.com>
2024-10-30 10:12:16 +0100

    Update llama_parsing.py

--- llama_parsing.py
+++ llama_parsing.py
@@ -2,10 +2,16 @@ import os
 import pdfplumber
 from groq import Groq
 
-client = Groq(api_key=os.environ.get("econometrics_parsing"),)
-def get_CIR (contenu) : 
+client = Groq(api_key="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")
+
+def get_CIR (file_path) : 
     with open(file_path, "r", encoding="utf-8") as fichier:
         contenu = fichier.read()  
-    chat_completion = client.chat.completions.create(messages=[{"role": "user", "content": "Quelle est le montant de crédit impôt recherche reçu par l'entreprise dont le bilan comptable est le suivant : {contenu}", }], model="llama3-8b-8192")
+    chat_completion = client.chat.completions.create(messages=[{"role": "user", "content": f"Quelle est le montant en euros de crédit impôt recherche reçu par l'entreprise dont le document suivant : {contenu}. Retourner uniquement le montant, sans explications.", }], model="llama-3.1-8b-instant")
     print(chat_completion.choices[0].message.content)
+
+get_CIR('/Users/eleonoregrison/Documents/2A/Econometrics/Econometrics/dossier_siren/500757901_2017-12-31.txt')
+
+
+
 
