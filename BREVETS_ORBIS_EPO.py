4d2fdf0e9c52f1a019d01eed41fb09816834bbec
davi-lucena-souza <davi_souza@usp.br>
2024-11-12 09:56:14 +0100

    Update BREVETS_ORBIS_EPO.py

--- BREVETS_ORBIS_EPO.py
+++ BREVETS_ORBIS_EPO.py
@@ -1,92 +1,98 @@
+# PATENT RETRIEVAL USING EPO API
 
-import pandas as pd
+# ATTENTION:
+"""
+1.  To use this code, it is necessary to have the Orbis database downloaded (`./resultados_patentes.csv`) 
+    and the base with probable SIREN (`./probable_CIR_ETI_GE.csv`), both available on GitHub.
 
-caminho_saida_csv = "./resultados_patentes.csv"  # CSV is in DRIVE
+2.  You should enter the first SIREN digit you want in the section marked as # FILTERING FOR THE DIGIT YOU WANT.
 
-# Carregar o arquivo CSV em um DataFrame
-df_orbis_siren = pd.read_csv(caminho_saida_csv, delimiter=';', low_memory=False)
+3. You can even run it in different terminals, but you should save to different CSV files. Be careful with the 
+   size of the loop to avoid retrieving duplicate patents, and then merge the two CSVs afterward.
 
-# Exibir o resultado
-df_orbis_siren
+   ex:
+   csv1_path = "./BREVETS_siren7(1).csv"
+   df1 = pd.read_csv(csv1_path, sep=';', dtype=str)
 
-#
-# Now I filtered the base to the Sirens that I want 
-# Obter a lista de SIRENs do DataFrame cir_dataEPO
-caminho_cir_data = "./probable_CIR_ETI_GE.csv"  # Substitua pelo caminho do arquivo correto
-df_cir_data = pd.read_csv(caminho_cir_data, low_memory=False)
+   csv2_path = "./BREVETS_siren7(2).csv"
+   df2 = pd.read_csv(csv2_path, sep=';', dtype=str)
 
-siren_list = df_cir_data['siren'].astype(str).tolist() # Make a siren_list using this df_cir_data1
+   df_combined = pd.concat([df1, df2], ignore_index=True)
+   df_combined.to_csv("BREVETS_siren7.csv", index=False, sep=';')  
+"""
 
-# Criar um conjunto de SIRENs que começam com 5 e adicionar "FR" na frente
-siren_filtered = ['FR' + siren for siren in siren_list if siren.startswith(('5'))] # Filter the the ones who starts with 5
-
-# Filtrar o DataFrame df_orbis_siren para manter apenas as linhas com appl_bvdid em siren_filtered
-df_filtered = df_orbis_siren[df_orbis_siren['appl_bvdid'].isin(siren_filtered)]
-
-# Salvar o DataFrame filtrado como CSV
-caminho_saida_csv = "./df_filtered.csv"  # Atualize com o caminho de saída desejado
-df_filtered.to_csv(caminho_saida_csv, index=False)
-df_filtered.reset_index(drop=True, inplace=True)
-
-
-# BIBLIOTECA
+# LIBRARY
 import pandas as pd
 import epo_ops
 import xml.etree.ElementTree as ET
 import os
 import re
+from requests.exceptions import ConnectionError
 
 
-# PATENTES DO 5
-caminho_saida_csv = "./df_filtered.csv"
-df_filtered = pd.read_csv(caminho_saida_csv, delimiter=';', low_memory=False)
+# LOADING ORBIS WITH ALL PATENTS
+caminho_saida_csv = "./resultados_patentes.csv"
+df_orbis_siren = pd.read_csv(caminho_saida_csv, delimiter=';', low_memory=False)
+
+
+# LOADING PROBABLE SIREN
+caminho_cir_data = "./probable_CIR_ETI_GE.csv"
+df_cir_data = pd.read_csv(caminho_cir_data, delimiter=";",low_memory=False)
+df_cir_data.columns = df_cir_data.columns.str.strip() 
+df_cir_data['siren'] = df_cir_data['siren'].astype(str).str.zfill(9)
+siren_list = df_cir_data['siren'].astype(str).tolist() 
+
+# FILTERING FOR THE DIGIT YOU WANT (in this examples is 7)
+siren_filtered = ['FR' + siren for siren in siren_list if siren.startswith(('7'))]
+df_filtered = df_orbis_siren[df_orbis_siren['appl_bvdid'].isin(siren_filtered)]
 df_filtered.reset_index(drop=True, inplace=True)
 
 
-# CHAVE EPO
-# Davi
-# client = epo_ops.Client(key="AhMvO8Kv17x0GszHaUnS9QGj8GVR92wU1ojTNEsFITirRLxL", secret="fgrznp1As3srmkG7k2TxAcAZBUXwp6F7MALDcRPy3pMNpldxADsJnH648NqytpUB")
-# Eleonore
-client = epo_ops.Client(key="3jaNanovIV5o7Eqd8AWSD8nQeH78hGUM09eu4NkJVgXyXaGN", secret="mjlvVoogucXmwTWLQL6CnaoOlgcBhYkaqjX2nAoDJbYafdmUGHGgjBsXVgXBGewu")
-
-# CSV where they will be saved
-csv_file_path = "./BREVETS_siren5.csv"
-
-
-# Verificar se o arquivo CSV existe e carregar os dados, se existir
+# CHOOSE THE NAME YOU WILL USE TO SAVE THE FOUND PATENTS IN CSV (in this case "BREVETS_siren7(1)")
+csv_file_path = "./BREVETS_siren7(1).csv"
+# Checking if the CSV file exists and load the data if it does.
 if os.path.exists(csv_file_path):
+    # Loading the existing DataFrame
     df_existente = pd.read_csv(csv_file_path, delimiter=';', low_memory=False)
 else:
     df_existente = pd.DataFrame(columns=["SIREN", "Famille ID", "Brevet", "Date de Dépôt", "Année de Dépôt"])
 
 
+# EPO KEY
+# Davi
+# client = epo_ops.Client(key="AhMvO8Kv17x0GszHaUnS9QGj8GVR92wU1ojTNEsFITirRLxL", secret="fgrznp1As3srmkG7k2TxAcAZBUXwp6F7MALDcRPy3pMNpldxADsJnH648NqytpUB")
+# Eleonore
+# client = epo_ops.Client(key="3jaNanovIV5o7Eqd8AWSD8nQeH78hGUM09eu4NkJVgXyXaGN", secret="mjlvVoogucXmwTWLQL6CnaoOlgcBhYkaqjX2nAoDJbYafdmUGHGgjBsXVgXBGewu")
+# Davi 2
+client = epo_ops.Client(key="tIQV3hg6ip0yqPy4mqAgynhzHrGsfklVAcen4yqJdAnWu6eK", secret="b2W0wFG7A2CuJX9THdiGpso0v3qkHuUgdLpI6hsYQzduopBY386AjNm8RC5BePzY")
+
+
 # TAILLE DU FOR
-start = 0
-interval = 10000
+start = 3765
+end = 100000
 
-for index, row in df_filtered.iloc[start:(start+interval)].iterrows():
+for index, row in df_filtered.iloc[start:end].iterrows():
 
-    # RESETANDO RESULTADOS
+    # RESETTING RESULTS AT EACH ITERATION TO SAVE AT THE END
     resultados = []
 
-    # PEGANDO SIRENE E PATENTE DA DF PATENTES DO 9 (df_filtered)
     siren = row['appl_bvdid'][2:]  
     numero_patente = row['pub_nbr']  
     
-    # PRINTANDO INDEX DA LINHA DA DF PATENTES DO 9 E A PATENTE
     print(f"{index} {numero_patente}")
-    
-    # Verificar se a patente já existe no DataFrame existente
+
+    # CHECK IF THE PATENT ALREADY EXISTS IN THE EXISTING DATAFRAME
     existing_row = df_existente[df_existente['Brevet'] == numero_patente]
-    
-    if not existing_row.empty: # df_existente já possui a linha
+    if not existing_row.empty: # df_existente already has the row
         if existing_row['Famille ID'].values[0] is not None and pd.notna(existing_row['Famille ID'].values[0]):
             print(f"   Existe déjà dans le fichier CSV avec une Famille ID valide.")
-            continue  # Pula para a próxima iteração se a patente já existir e Famille ID for válido
+            continue  # Skip to the next iteration if the patent already exists and the Famille ID is valid.
         else:
             print(f"   Existe déjà dans le fichier CSV, mais Famille ID est manquant. Tentando novamente...")
-            idx = existing_row.index[0]  # Pegando o índice da primeira linha correspondente
+            idx = existing_row.index[0]  # Getting the index of the first matching row.
 
+
+    # FORMATTING PATENT NAME FOR API SEARCH
     codigo_pais = re.match(r'([A-Z]+)', numero_patente).group(1)
     remaining = numero_patente[len(codigo_pais):]  
     match = re.search(r'(\d+)([A-Z]{1,2}[0-9]?)', remaining) 
@@ -97,31 +103,30 @@ for index, row in df_filtered.iloc[start:(start+interval)].iterrows():
         numero_docdb = remaining
         tipo_demanda = ''
 
+
     try:
-        # Buscar informações da patente na API do EPO
+        # RETRIEVING PATENT INFORMATION FROM THE EPO API
         response = client.published_data(
             reference_type='publication',
             input=epo_ops.models.Docdb(numero_docdb, codigo_pais, tipo_demanda),
             endpoint='biblio',
         )
 
-        # Processar a resposta para extrair as informações
+        # Process the response to extract the information
         if response.status_code == 200:
             data = response.text
-            root = ET.fromstring(data)  # Analisar o XML
+            root = ET.fromstring(data)  
 
-            # Navegue até os elementos que contêm as informações necessárias
+            # Navigate to the elements that contain the necessary information
             namespace = {'ops': 'http://ops.epo.org', 'default': 'http://www.epo.org/exchange'}
 
-            # Obtendo as informações
+            # Getting the information
             famille_id = root.find('.//default:exchange-document', namespaces=namespace).get('family-id', 'N/A')
             date_depot = root.find('.//default:application-reference/default:document-id/default:date', namespaces=namespace).text if root.find('.//default:application-reference/default:document-id/default:date', namespaces=namespace) is not None else 'N/A'
-
-            # Extraindo o ano da data de depósito
             ano_depot = date_depot[:4] if date_depot != 'N/A' else 'N/A'
 
-            # Atualizando ou adicionando os resultados
-            if existing_row.empty: # se for patente nova
+            # Updating or adding the results
+            if existing_row.empty: # If it's a new patent
                 resultados.append({
                     "SIREN": siren,
                     "Famille ID": famille_id,
@@ -131,22 +136,22 @@ for index, row in df_filtered.iloc[start:(start+interval)].iterrows():
                 })
             else:
                 idx = existing_row.index[0]
-                # Atualizando a linha existente
+                # Updating the existing row
                 df_existente.at[idx, 'Famille ID'] = famille_id
                 df_existente.at[idx, 'Date de Dépôt'] = date_depot
                 df_existente.at[idx, 'Année de Dépôt'] = ano_depot
 
             if not existing_row.empty and famille_id == 'N/A':
-                print(f"   {numero_patente} não foi encontrada denovo, mesmo sendo response = 200. Pula para a próxima iteração.")
-                continue  # Pula para a próxima iteração se a patente já existir e Famille ID estiver ausente
+                print(f"   {numero_patente} NOT found again, even with response = 200. Skip to the next iteration.")
+                continue  # Skip to the next iteration if the patent already exists and the Famille ID is missing
 
         else:
 
             if not existing_row.empty and (existing_row['Famille ID'].values[0] is None or pd.isna(existing_row['Famille ID'].values[0])):
-                print(f"{numero_patente} não foi encontrada denovo, response NÃO FOI 200. Pula para a próxima iteração.")
-                continue  # Pula para a próxima iteração se a patente já existir e Famille ID estiver ausente
+                print(f"{numero_patente} Not found again, response was NOT 200. Skip to the next iteration.")
+                continue  # Skip to the next iteration if the patent already exists and the Famille ID is missing.
 
-            # Quando a patente não é encontrada
+            # When the patent is not found
             print(f"   Erreur lors de la recherche du brevet {numero_patente}: {response.status_code} - {response.text}")
             resultados.append({
                 "SIREN": siren,
@@ -159,10 +164,10 @@ for index, row in df_filtered.iloc[start:(start+interval)].iterrows():
     except Exception as e:
         print(f"   Erreur lors du traitement du brevet {numero_patente}: {e}")
         if not existing_row.empty and (existing_row['Famille ID'].values[0] is None or pd.isna(existing_row['Famille ID'].values[0])):
-            print(f"   {numero_patente} não foi encontrada denovo, deu EXCEPT. Pula para a próxima iteração.")
-            continue  # Pula para a próxima iteração se a patente já existir e Famille ID estiver ausente
+            print(f"   {numero_patente} Not found again, raised EXCEPT. Skip to the next iteration.")
+            continue  # Skip to the next iteration if the patent already exists and the Famille ID is missing
 
-        # Adiciona as informações disponíveis, com campos em branco
+        # Add the available information, with blank fields.
         resultados.append({
             "SIREN": siren,
             "Famille ID": None,
@@ -171,39 +176,39 @@ for index, row in df_filtered.iloc[start:(start+interval)].iterrows():
             "Année de Dépôt": None
         })
 
-    # Zerando o DataFrame
+
+    # Converting the found results into a DataFrame
     df_resultados = pd.DataFrame(columns=["SIREN", "Famille ID", "Brevet", "Date de Dépôt", "Année de Dépôt"])
-    
-    # Criar um DataFrame com os resultados
     df_resultados = pd.DataFrame(resultados)
 
-    # Verifique se a coluna 'Année de Dépôt' existe e não está vazia
+
+    # Checking if the 'Année de Dépôt' column exists and is not empty
     if 'Année de Dépôt' in df_resultados.columns and not df_resultados['Année de Dépôt'].empty:
-        # Criar colunas para os anos de 2016 a 2024
+        # Create columns for the years 2016 to 2024
         for year in range(2016, 2025):
             df_resultados[str(year)] = (df_resultados['Année de Dépôt'] == str(year)).astype(int)
     
-    # Tentar formatar a data de depósito como dd/mm/aaaa, ignorando se a coluna não existir
+
+    # FORMATTING COLUMNS
+    # Try formatting the deposit date as dd/mm/yyyy, ignoring if the column does not exist
     try:
         df_resultados['Date de Dépôt'] = pd.to_datetime(df_resultados['Date de Dépôt'], errors='coerce').dt.strftime('%d/%m/%Y').where(df_resultados['Date de Dépôt'].notnull())
     except KeyError:
         print("   .")
-    
-    # Salvar o DataFrame atualizado em um arquivo CSV, formatando o SIREN
-    if 'SIREN' in df_resultados.columns:  # Verifique se a coluna SIREN existe
-        df_resultados['SIREN'] = df_resultados['SIREN'].str.zfill(9)  # Preencher com zeros à esquerda
+    # Formatting SIREN
+    if 'SIREN' in df_resultados.columns:  
+        df_resultados['SIREN'] = df_resultados['SIREN'].str.zfill(9)  
         df_resultados['SIREN'] = df_resultados['SIREN'].str.replace(r'(\d{3})(\d{3})(\d{3})', r'\1 \2 \3', regex=True)  # Adicionar espaços
-
-    # Verifica se a coluna 'Famille ID' existe antes de convertê-la
+    # Check if the 'Famille ID' column exists before converting it in str
     if 'Famille ID' in df_resultados.columns:
         df_resultados['Famille ID'] = df_resultados['Famille ID'].astype(str)
-
-    # Verifica se a coluna 'Année de Dépôt' existe antes de convertê-la
+    # Check if the 'Année de Dépôt' column exists before converting it
     if 'Année de Dépôt' in df_resultados.columns:
         df_resultados['Année de Dépôt'] = df_resultados['Année de Dépôt'].astype(str)
 
-    # Adicionar os novos resultados ao DataFrame existente
+
+    # Add the new results to the existing DataFrame
     df_existente = pd.concat([df_existente, df_resultados], ignore_index=True)
 
-    # Salvar o DataFrame atualizado em um arquivo CSV
+    # Save the updated DataFrame to a CSV file
     df_existente.to_csv(csv_file_path, index=False, sep=';')
